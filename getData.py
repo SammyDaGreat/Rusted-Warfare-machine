6e2f160b3539dc67cbfeea8dc3e909a4b3d3ff89
SammyDaGreat <sammy5673@hotmail.com>
2019-05-17 22:12:54 +0800

    Optimized getData.py

--- getData.py
+++ getData.py
@@ -4,20 +4,17 @@ import cv2
 import time,math,os,json
 import win32gui, win32ui, win32con, win32api    # win32 libs
 
-file_iter = 0
-WIDTH = 960
-HEIGHT = 940
-
-### Adjust UUID before running ###
-
-uuid = -1
+uuid_str = input('Specify UUID of data:\t')
+if uuid_str == '':
+	uuid = -1
+elif uuid_str == 't':
+	uuid = -99
+else:
+	uuid = eval(uuid_str)
 
 # Thoughts:
-#	Should I record mouse pos all the time or just when clicking? 
 #	How to distinguish clicking and dragging?
 #	Higher fps?
-
-### Adjust UUID before running ###
 
 def is_window(name):
 	title = win32gui.GetWindowText(win32gui.GetForegroundWindow())
@@ -40,27 +37,32 @@ def save_y(n,img,mouse,kb):
 	np.save('Data\\y1\\'+file_name,mouse_array)
 	np.save('Data\\y2\\'+file_name,kb_array)
 
+std_dt = 0.015
 def replay(that_uuid):
 	try:
 		i=0
+		last_time = time.time()
 		while(True):
-				time.sleep(0.1)
-				shot = np.load('Data//x//'+str(that_uuid)+'.'+str(i)+'.npy')
-				cv2.imshow('window',shot)
-				mouse_pos = np.load('Data//y1//'+str(that_uuid)+'.'+str(i)+'.npy')
-				kb = np.load('Data//y2//'+str(that_uuid)+'.'+str(i)+'.npy')
-				i += 1
-				if cv2.waitKey(25)&0xFF == ord('q'):
-					cv2.destroyAllWindows()
-					break
+			while (time.time() - last_time) < std_dt:
+           		time.sleep(0.001)
+        	last_time = time.time()
+
+			shot = np.load('Data//x//'+str(that_uuid)+'.'+str(i)+'.npy')
+			cv2.imshow('window',shot)
+			mouse_pos = np.load('Data//y1//'+str(that_uuid)+'.'+str(i)+'.npy')
+			kb = np.load('Data//y2//'+str(that_uuid)+'.'+str(i)+'.npy')
+			i += 1
+			if cv2.waitKey(25)&0xFF == ord('q'):
+				cv2.destroyAllWindows()
+				break
 	except:
+		print('Replay ended.')
 		return
 
 f = open('VK_CODE','r')
 VK_CODE = json.load(f)
 f.close()
 vk = [VK_CODE[key] for key in VK_CODE]
-
 
 def get_kb():
 	kb = [0,0]
@@ -81,7 +83,9 @@ def get_kb():
 
 # Returns standardized mouse pos
 ## Should I actually standardize here?
-def get_mouse():
+def get_mouse(kb):
+	if kb[0] == 0 and kb[1] == 0:
+		return [0,0]
 	cords = win32gui.GetCursorPos()
 	mouse_pos = [cords[0]/WIDTH,cords[1]/HEIGHT]
 	return mouse_pos
@@ -92,20 +96,25 @@ if uuid==-1:
 		uuid_init += 1
 	uuid = uuid_init
 
-# Main body
+file_iter = 0
+WIDTH = 960
+HEIGHT = 940
+
+# Main
 #replay(0)
 last_time = time.time()
 while(True):
 	while(win32api.GetKeyState(112)>=0) or not is_rusted():
-		time.sleep(0.1)
+		time.sleep(0.001)
 	while(win32api.GetKeyState(113)>=0) and is_rusted():
-		time.sleep(0.015)
+		while (time.time() - last_time) < std_dt:
+            time.sleep(0.001)
+        last_time = time.time()
+
+		kb = get_kb()
+		mouse_pos = get_mouse(kb)
 		screen = np.array(ImageGrab.grab(bbox=(0,40,WIDTH,40+HEIGHT)))
 		new_screen = process_img(screen)
-		last_time = time.time()
-
-		mouse_pos = get_mouse()
-		kb = get_kb()
 
 		if uuid >= 0:
 			save_y(file_iter,new_screen,mouse_pos,kb)
